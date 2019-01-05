9d8113f7d9d3b9c946a0d4aed01b3e776ca5568f
JekaLion <djoni-01@mail.ru>
2019-01-05 10:34:11 +0600

    Signed-off-by: JekaLion <djoni-01@mail.ru>

---
+++ ViewsLesson/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewsLesson.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.MessageText = "Its text LUL";
+            return View();
+        }
+    }
+}
---
+++ ViewsLesson/Models/SportContext.cs
@@ -0,0 +1,17 @@
+﻿namespace ViewsLesson.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class SportContext : DbContext
+    {
+        public SportContext()
+            : base("name=SportContext")
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+    }
+}
